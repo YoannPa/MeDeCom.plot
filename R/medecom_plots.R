37da1a45ce26c6a5f84e7486698f20b51a8e406b
Pageaud <pageaud@odcf-worker01.inet.dkfz-heidelberg.de>
2022-06-30 15:03:33 +0200

    Issue fixed in the core error plot function.

--- R/medecom_plots.R
+++ R/medecom_plots.R
@@ -52,7 +52,7 @@ errorplot3d <- function(
   #Get valleys for each lambda distributions
   dt_valleys <- dt_err[
     order(variable)][, quantmod::findValleys(value), by = variable]
-  if(nrow(dt_valleys) > 1){
+  if(nrow(dt_valleys) > 0){
     #Get error values for each valleys
     minimas <- unlist(lapply(X = seq(nrow(dt_valleys)), FUN = function(i){
       dt_err[
@@ -113,6 +113,7 @@ errorplot3d <- function(
       text = list(lab = dt_minimas$legend_keys, cex = 0.95))
   } else { minimas_legend <- NULL }
   # Error plot
+  cat("TEST")
   err.plt <- lattice::wireframe(
     x = Error ~ Lambda * Kappa, data = dt_err, drape = TRUE, colorkey = TRUE,
     screen = list(z = -135, x = -80, y = 0), at = sort(dt_err$Error),
